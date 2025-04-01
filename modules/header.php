5d44788dabd92925154f1baa3d367c0bcea7aafe
Sem <6044654@mborijnland.nl>
2025-04-01 09:25:43 +0200

    PHP links

--- modules/header.php
+++ modules/header.php
@@ -4,6 +4,11 @@
         <nav class="navagationHolder">
             <section class="navtext">
                 <?php
+                
+                // links waar ik dingen heb gevonden:
+                // https://www.w3schools.com/Php/func_filesystem_basename.asp base name function
+                // https://www.w3schools.com/php/php_operators.asp#:~:text=Try%20it-,%3F%3A,-Ternary de ? button!
+                
                 // maakt site waar hij op zit bolded/strong
                     function navagationBolder($page, $text){
                         // basenamt pakt de file name+ extention dus bvb "menu.php"
